22560ffcc675da8b0999d8436dbc8683dbe36f56
leo boone <lb8721@bard.edu>
2026-04-24 09:55:26 -0400

    pt2

--- PrefixTree.java
+++ PrefixTree.java
@@ -9,12 +9,34 @@ public class PrefixTree<Value extends Comparable<Value> >{
 
         public Node(Value val) {
             this.val = val;
+            this.left=null;
+            this.right=null;
         }
     }
+//constructor that reads preorder traversal and reconstructs tree
+public PrefixTree(String preorder){
+    this.root=buildTree(preorder, new int[]{0});
+}
+private Node buildTree(preorder, int[]index);{
+    if (index[0]>=preorder.length()){
+        return null;
+    }
 
+  char ch = preorder.charAt(index[0]);
+    index[0]++;
     
+    Node node = new Node(ch);
+
+    if (ch == '*') {
+        node.left = buildTree(preorder, index);
+        node.right = buildTree(preorder, index);
+    }
+    
+    return node;
+
+}
     public PrefixTree() {
-        // empty BST
+        private Node root;
     }
 
     private Value get(Node x, Value val) {
@@ -27,5 +49,8 @@ public class PrefixTree<Value extends Comparable<Value> >{
             return get(x.right, val);
         else
             return x.val;
+    }
+    private Node BuildTree(preorder, int[]index){
+        
     }
 }
